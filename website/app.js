9129eebfa95cdbcf11fd39c3cb7ea4f9e38b262c
Unknown <tglaubitz@gmx.de>
2020-03-20 10:29:34 +0100

    feat: implement post and get methods

--- website/app.js
+++ website/app.js
@@ -9,3 +9,32 @@ const apiUrl = (zip, apiKey) =>
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// GET method to query weather
+const getWeather = async (owmUrl, zip, apiKey) => {
+    const apiResponse = await fetch(owmUrl(zip, apiKey));
+    try {
+        const weather = await apiResponse.json();
+        return weather;
+    } catch (error) { console.log(`Failed to get weather: ${error}`) };
+};
+
+// POST method to backend server
+const postData = async (url = '', data = {}) => {
+    try {
+	const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+		'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+	});
+        const newData = await response.json();
+        console.log(newData);
+        return newData
+    } catch(error) {
+	console.log("the error is", error);
+	// appropriately handle the error
+    }; 
+};
