2f497812af20f0058b96804fc87d10651f5f935a
Unknown <tglaubitz@gmx.de>
2020-03-20 10:23:47 +0100

    feat: set up openweather api

--- website/app.js
+++ website/app.js
@@ -1,4 +1,10 @@
-/* Global Variables */
+// Client side code
+
+// Global Variables - set up the OpenWeather API
+const apiKey = '12519067d9a80da46f35d8e349bf4cab';
+const apiUrl = (zip, apiKey) =>
+      'https://api.openweathermap.org/data/2.5/weather' +
+      `?zip=${zip},us&units=imperial&appid=${apiKey}`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
